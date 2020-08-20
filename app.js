52e3737b2475333fe26efb70287fd57922278935
ruscat <rus.reg.36@gmail.com>
2020-08-20 09:01:14 +0300

    048

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+let scores;
+let roundScore;
+let activePlayer;
+let dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(`#score-${activePlayer}`).textContent = dice;
+const x = document.querySelector(`#score-0`).textContent;
+
+document.querySelector('.dice').style.display = 'none';
